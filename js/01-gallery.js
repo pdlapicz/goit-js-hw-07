257d8f0c1ea28070571648a2583cc5da5f22c5e8
Przemyslaw Daniel Lapicz <przemyslaw.lapicz@gmail.com>
2023-08-15 19:57:04 +0200

    add changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,56 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 let lightbox;
 
-const galleryItem = ({ preview, original, description }) => `
-    <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}" 
-            data-source="${original}" 
-            alt="${description}" />
-        </a>
-    </div>
+const divItem = ({ preview, original, description }) => `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
 `;
 
-const getImages = (img) => img.map((item) => galleryItem(item)).join('');
+const getImages = (img) => img.map((item) => divItem(item)).join("");
 
 gallery.innerHTML = getImages(galleryItems);
 
-gallery.addEventListener('click', onClickGallery);
+gallery.addEventListener("click", onClickGallery);
 
 const genBigImg = ({ url, description }) => `
-    <img
-        class="gallery__image"
-        src="${url}"
-        width=1280
-        alt="${description}"/>
+  <img
+    class="gallery__image"
+    src="${url}"
+    width="1280"
+    alt="${description}"/>
 `;
 
 function onClickGallery(e) {
   e.preventDefault();
 
-  const url = e.targer.dataset.source;
+  const url = e.target.dataset.source;
   const description = e.target.alt;
 
   if (!(url && description)) return;
 
   lightbox = basicLightbox.create(genBigImg({ url, description }), {
     onClose: () => {
-      gallery.removeEventListener('keydown', onKeyDownEsc);
+      gallery.removeEventListener("keydown", onKeyDownEsc);
     },
   });
   lightbox.show();
-  gallery.addEventListener('keydown', onKeyDownEsc);
+  gallery.addEventListener("keydown", onKeyDownEsc);
 }
 
+// procedura zamkniecia podgladu za pomoca klawisza ESC
 const onKeyDownEsc = (e) => {
-  if (e.key === 'Escape' || e.keyCode === 27) {
+  if (e.key === "Escape" || e.keyCode === 27) {
     lightbox.close();
   }
   console.log(e.key);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+const liItem = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+  </li>
+`;
+
+const getImages = (img) => img.map((item) => liItem(item)).join('');
+
+gallery.innerHTML = getImages(galleryItems);
+
+gallery.addEventListener('click', onClickGallery);
+
+// utworzenie instancji: podpis pod obrazkiem po czasie 250 ms
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onClickGallery(e) {
+  e.preventDefault();
+}
+
 console.log(galleryItems);
